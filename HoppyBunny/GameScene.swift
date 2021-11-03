f85b8ca5e4833571fe5949bfee88db711ed1f04e
Alberto <aldo10012@gmail.com>
2021-11-03 13:37:15 -0700

    first commit

---
+++ HoppyBunny/GameScene.swift
@@ -0,0 +1,66 @@
+//
+//  GameScene.swift
+//  HoppyBunny
+//
+//  Created by Alberto Dominguez on 10/25/21.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    var hero: SKSpriteNode!
+    var sinceTouch : CFTimeInterval = 0
+    let fixedDelta: CFTimeInterval = 1.0 / 60.0 /* 60 FPS */
+
+
+    
+    override func didMove(to view: SKView) {
+        /* Setup your scene here */
+        
+        /* Recursive node search for 'hero' (child of referenced node) */
+        hero = (self.childNode(withName: "//hero") as! SKSpriteNode)
+        
+        /* allows the hero to animate when it's in the GameScene */
+        hero.isPaused = false
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        /* Called when a touch begins */
+        
+        /* Apply vertical impulse */
+        hero.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 300))
+        
+        /* Apply subtle rotation */
+        hero.physicsBody?.applyAngularImpulse(1)
+        
+        /* Reset touch timer */
+        sinceTouch = 0
+    }
+
+    override func update(_ currentTime: TimeInterval) {
+        /* Called before each frame is rendered */
+        
+        /* Grab current velocity */
+        let velocityY = hero.physicsBody?.velocity.dy ?? 0
+        
+        /* Check and cap vertical velocity */
+        if velocityY > 400 {
+            hero.physicsBody?.velocity.dy = 400
+        }
+        
+        /* Apply falling rotation */
+        if sinceTouch > 0.2 {
+            let impulse = -20000 * fixedDelta
+            hero.physicsBody?.applyAngularImpulse(CGFloat(impulse))
+        }
+        
+        /* Clamp rotation */
+        hero.zRotation.clamp(v1: CGFloat(-90).degreesToRadians(), CGFloat(30).degreesToRadians())
+        hero.physicsBody?.angularVelocity.clamp(v1: -1, 3)
+        
+        /* Update last touch timer */
+        sinceTouch += fixedDelta
+    }
+}
